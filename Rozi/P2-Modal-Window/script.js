df963ffdfeeb569ec28dd5a5724dbf0de227b59b
Ruzina Khatoon <ruzinakhatoon@gmail.com>
2021-09-25 20:48:33 -0400

    Modal Window

---
+++ Rozi/P2-Modal-Window/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let allButtons = document.querySelectorAll(".show-modal");
+let modal = document.querySelector(".modal");
+let closeElement = document.querySelector(".close-modal");
+let overlayElement = document.querySelector(".overlay");
+
+
+for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener("click", function() {
+        modal.classList = "modal";
+        overlayElement.className = "overlay";
+    });    
+}
+
+const closeModal = () => {
+    modal.classList = "modal hidden";
+    overlayElement.className = "overlay hidden";
+};
+
+closeElement.addEventListener("click", closeModal);
+overlayElement.addEventListener("click", closeModal); 
+
+//keyboard
+document.addEventListener("keydown",function(event) {
+    if (event.key == "Escape") {
+        closeModal();
+    }
+});
+

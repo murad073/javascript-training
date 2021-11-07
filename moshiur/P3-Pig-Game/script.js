92b7c5a2d553d33bcf0919a79538b22c8f2a3f81
Moshiur Rahman <mrahman@I-360.LOCAL>
2021-11-07 04:59:15 -0500

    pig game done

--- moshiur/P3-Pig-Game/script.js
+++ moshiur/P3-Pig-Game/script.js
@@ -1,34 +1,76 @@
 'use strict';
 
-let player = 1;
-let player1CurrentValue = 0, player2CurrentValue = 0;
+let currentPlayer = 0; // index based 
+let currentValues = [0, 0, 0];
+let totalScores = [0, 0, 0];
+const WIN_SCORE = 20;
 const rollDiceButton = document.querySelector(".btn--roll");
 const diceImage = document.querySelector(".dice");
-const player1CurrentValueElement = document.querySelector("#current--0");
-const player2CurrentValueElement = document.querySelector("#current--1");
-const player1ScoreElement = document.querySelector("#score--0");
-const player2ScoreElement = document.querySelector("#score--1");
+const holdButton = document.querySelector(".btn--hold");
 
 rollDiceButton.addEventListener('click', () => {
     // get a random number between 1-6
     let num = Math.floor(Math.random() * 6) + 1;
+
     // update the image element with the image 
     let attr = diceImage.attributes.getNamedItem("src");
     attr.value = `dice-${num}.png`;
     diceImage.classList.remove("hidden");
 
-    if (player === 1) {
-        player1CurrentValue += num;
-        player1CurrentValueElement.textContent = player1CurrentValue;
-    } else if (player === 2) {
-        player2CurrentValue += num;
-        player2CurrentValueElement.textContent = player2CurrentValue;
+    if (num === 1) {
+        currentValues[currentPlayer] = 0; 
+        document.querySelector("#current--" + currentPlayer).textContent = currentValues[currentPlayer];
+        switchToNextPlayer();
+    } else {
+        currentValues[currentPlayer] += num;
+        document.querySelector("#current--" + currentPlayer).textContent = currentValues[currentPlayer];
     }
 });
 
+holdButton.addEventListener("click", () => {
+    totalScores[currentPlayer] += currentValues[currentPlayer];
+    document.querySelector("#score--" + currentPlayer).textContent = totalScores[currentPlayer];
+    currentValues[currentPlayer] = 0;
+    document.getElementById(`current--${currentPlayer}`).textContent = currentValues[currentPlayer];
+
+    if (totalScores[currentPlayer] >= WIN_SCORE) {
+        document.querySelector(`#name--${currentPlayer}`).textContent = `PLAYER ${currentPlayer+1} Win`;
+        document.querySelector(".player--" + currentPlayer).classList.add("player--winner");
+        rollDiceButton.classList.add("hidden");
+        holdButton.classList.add("hidden");
+        diceImage.classList.add("hidden");
+    } else {
+        switchToNextPlayer();
+    }
+});
+
+document.querySelector(".btn--new").addEventListener("click", initialCleanup);
+
+var switchToNextPlayer = () => {
+    let nextIndex = currentPlayer + 1;
+    if (nextIndex >= totalScores.length) nextIndex = 0;
+    document.querySelector(".player--" + currentPlayer).classList.remove("player--active");
+    document.querySelector(".player--" + nextIndex).classList.add("player--active");
+    diceImage.classList.add("hidden");
+    currentPlayer = nextIndex;
+};
 
 // initial cleanup 
-player1ScoreElement.textContent = 0;
-player2ScoreElement.textContent = 0;
-diceImage.classList.add("hidden");
+function initialCleanup() {
+    currentPlayer = 0;
+    for(let i=0; i<totalScores.length; i++) {
+        totalScores[i] = 0;
+        currentValues[i] = 0;
+        document.querySelector("#score--" + i).textContent = totalScores[i];
+        document.querySelector(`#current--${i}`).textContent = currentValues[i];
+        document.querySelector(".player--" + i).classList.remove("player--active");
+        document.querySelector(".player--" + i).classList.remove("player--winner");
+    }
 
+    diceImage.classList.add("hidden");
+    holdButton.classList.remove("hidden");
+    rollDiceButton.classList.remove("hidden");
+    document.querySelector(".player--" + currentPlayer).classList.add("player--active");
+}
+
+initialCleanup();
